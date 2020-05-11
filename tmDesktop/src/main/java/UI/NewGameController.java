488619a318177d76832f560051e167c63caa0f03
iremsvn <iremsvn@gmail.com>
2020-05-12 01:35:46 +0300

    New Game scene improvements made
    Choosing player numbers and faction names were mostly made. Style improved.

--- tmDesktop/src/main/java/UI/NewGameController.java
+++ tmDesktop/src/main/java/UI/NewGameController.java
@@ -1,32 +1,35 @@
 package UI;
 
-import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-import javax.print.DocFlavor;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NewGameController implements Initializable {
 
 	@FXML public Button fullScreenButton;
+	@FXML public VBox factionChooseBox;
+	@FXML public HBox playerNumberBox;
+	@FXML public ChoiceBox<String> choiceBox0;
+	@FXML public ChoiceBox<String> choiceBox1;
+	@FXML public ChoiceBox<String> choiceBox2;
+	@FXML public ChoiceBox<String> choiceBox3;
+	@FXML public ChoiceBox<String> choiceBox4;
+	@FXML public ChoiceBox<String> choiceBox5;
+	@FXML public HBox factionBox3;
+	@FXML public HBox factionBox4;
+	@FXML public HBox factionBox5;
 
 	public NewGameController(){
 
@@ -36,29 +39,35 @@ public class NewGameController implements Initializable {
 	 * 
 	 * @param event
 	 */
-	private void startGameClicked(MouseEvent event) {
-		// TODO - implement NewGameController.startGameClicked
-		throw new UnsupportedOperationException();
+	@FXML
+	private void startGameClicked(ActionEvent event) throws Throwable {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation((new java.io.File("src/main/java/UI/view/Gameplay.fxml")).toURI().toURL());
+		Scene scene = new Scene(loader.load());
+		GameUI.stage.setScene(scene);
+		GameUI.stage.setFullScreen(true);
 	}
-
 
 
 	/**
 	 * 
 	 * @param event
 	 */
-	public void confirmPlayerNoClicked(ActionEvent event) {
-		// TODO - implement NewGameController.confirmPlayerNoClicked
-		throw new UnsupportedOperationException();
+	@FXML
+	public void confirmPlayerNoClicked(ActionEvent event) throws Throwable {
+		playerNumberBox.setVisible(false);
+		factionChooseBox.setVisible(true);
 	}
+
 
 	/**
 	 * 
 	 * @param event
 	 */
-	public void changePlayerNoClicked(ActionEvent event) {
-		// TODO - implement NewGameController.changePlayerNoClicked
-		throw new UnsupportedOperationException();
+	@FXML
+	public void changePlayerNoClicked(ActionEvent event) throws Throwable {
+		playerNumberBox.setVisible(true);
+		factionChooseBox.setVisible(false);
 	}
 
 
@@ -79,6 +88,7 @@ public class NewGameController implements Initializable {
 		GameUI.stage.setFullScreen(true);
 	}
 
+
 	/**
 	 *
 	 * @param event
@@ -95,5 +105,12 @@ public class NewGameController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 			fullScreenButton.setVisible(false);
+			factionChooseBox.setVisible(false);
+			choiceBox0.setItems(FXCollections.observableArrayList("2", "3", "4", "5"));
+			choiceBox1.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox2.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox3.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox4.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox5.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
 	}
 }
