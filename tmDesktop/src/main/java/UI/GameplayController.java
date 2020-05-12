8c58f37599bd1e09ee741a803536f1c770fdf2ee
iremsvn <iremsvn@gmail.com>
2020-05-12 21:17:10 +0300

    New Game page completed, Gameplay scene template made
    The New Game page now opens Gameplay. Back button and fullscreen button functionalities implemented in the Gameplay scene also.

--- tmDesktop/src/main/java/UI/GameplayController.java
+++ tmDesktop/src/main/java/UI/GameplayController.java
@@ -1,15 +1,25 @@
 package UI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 
 import javax.print.DocFlavor;
+import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GameplayController {
+public class GameplayController  implements Initializable {
+
+	@FXML  public Button backButton;
+	@FXML public Button fullScreenButton;
 
 	private Polygon[] trains;
 	private Button chooseAction;
@@ -21,6 +31,7 @@ public class GameplayController {
 	private Pane playerInfo;
 	private Label factionName;
 	private Label playerName;
+
 
 	/**
 	 * 
@@ -104,14 +115,40 @@ public class GameplayController {
 		throw new UnsupportedOperationException();
 	}
 
+
+	public void handle(KeyEvent t) {
+		if(t.getCode()== KeyCode.ESCAPE)
+		{
+			fullScreenButton.setVisible(true);
+		}
+	}
+
 	/**
-	 * 
-	 * @param url
-	 * @param resourceBundle
+	 *
+	 * @param event
 	 */
-	public void initialize(DocFlavor.URL url, ResourceBundle resourceBundle) {
-		// TODO - implement GameplayController.initialize
-		throw new UnsupportedOperationException();
+	@FXML
+	private void fullScreenButtonClicked(ActionEvent event) throws Throwable {
+		fullScreenButton.setVisible(false);
+		GameUI.stage.setFullScreen(true);
+	}
+
+	/**
+	 *
+	 * @param event
+	 */
+	@FXML
+	private void backButtonClicked(ActionEvent event) throws Throwable {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation((new java.io.File("src/main/java/UI/view/MainMenu.fxml")).toURI().toURL());
+		Scene scene = new Scene(loader.load());
+		GameUI.stage.setScene(scene);
+		GameUI.stage.setFullScreen(true);
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		fullScreenButton.setVisible(false);
 	}
 
 }
--- tmDesktop/src/main/java/UI/NewGameController.java
+++ tmDesktop/src/main/java/UI/NewGameController.java
@@ -14,14 +14,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class NewGameController implements Initializable {
 
 	@FXML public Button fullScreenButton;
+	@FXML public Button confirmPlayerNoButton;
 	@FXML public VBox factionChooseBox;
 	@FXML public HBox playerNumberBox;
-	@FXML public ChoiceBox<String> choiceBox0;
+	@FXML public ChoiceBox<Integer> choiceBox0;
 	@FXML public ChoiceBox<String> choiceBox1;
 	@FXML public ChoiceBox<String> choiceBox2;
 	@FXML public ChoiceBox<String> choiceBox3;
@@ -30,6 +32,7 @@ public class NewGameController implements Initializable {
 	@FXML public HBox factionBox3;
 	@FXML public HBox factionBox4;
 	@FXML public HBox factionBox5;
+	int playerNumber;
 
 	public NewGameController(){
 
@@ -41,6 +44,9 @@ public class NewGameController implements Initializable {
 	 */
 	@FXML
 	private void startGameClicked(ActionEvent event) throws Throwable {
+		if(!isValid()){
+			return;
+		}
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation((new java.io.File("src/main/java/UI/view/Gameplay.fxml")).toURI().toURL());
 		Scene scene = new Scene(loader.load());
@@ -48,6 +54,21 @@ public class NewGameController implements Initializable {
 		GameUI.stage.setFullScreen(true);
 	}
 
+	private boolean isValid(){
+		String[] arr = new String[5];
+		arr[0] = choiceBox1.getValue();
+		arr[1] = choiceBox2.getValue();
+		arr[2] = choiceBox3.getValue();
+		arr[3] = choiceBox4.getValue();
+		arr[4] = choiceBox5.getValue();
+		for(int i = 0; i < playerNumber - 1; i++){
+			for(int j = i + 1; j < playerNumber; j++){
+				if(arr[i].equals(arr[j]))
+					return false;
+			}
+		}
+		return true;
+	}
 
 	/**
 	 * 
@@ -55,8 +76,33 @@ public class NewGameController implements Initializable {
 	 */
 	@FXML
 	public void confirmPlayerNoClicked(ActionEvent event) throws Throwable {
+		getPlayerNoChoice(choiceBox0);
 		playerNumberBox.setVisible(false);
 		factionChooseBox.setVisible(true);
+	}
+
+	private void getPlayerNoChoice(ChoiceBox<Integer> choiceBox){
+		playerNumber = choiceBox.getValue();
+		if(playerNumber == 2) {
+			factionBox3.setVisible(false);
+			factionBox4.setVisible(false);
+			factionBox5.setVisible(false);
+		}
+		if(playerNumber == 3) {
+			factionBox3.setVisible(true);
+			factionBox4.setVisible(false);
+			factionBox5.setVisible(false);
+		}
+		if(playerNumber == 4) {
+			factionBox3.setVisible(true);
+			factionBox4.setVisible(true);
+			factionBox5.setVisible(false);
+		}
+		if(playerNumber == 5) {
+			factionBox3.setVisible(true);
+			factionBox4.setVisible(true);
+			factionBox5.setVisible(true);
+		}
 	}
 
 
@@ -106,11 +152,17 @@ public class NewGameController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 			fullScreenButton.setVisible(false);
 			factionChooseBox.setVisible(false);
-			choiceBox0.setItems(FXCollections.observableArrayList("2", "3", "4", "5"));
+			choiceBox0.setItems(FXCollections.observableArrayList(2, 3, 4, 5));
+			choiceBox0.setValue(2);
 			choiceBox1.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox1.setValue("a");
 			choiceBox2.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox2.setValue("a");
 			choiceBox3.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox3.setValue("a");
 			choiceBox4.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox4.setValue("a");
 			choiceBox5.setItems(FXCollections.observableArrayList("a", "b", "c", "d"));
+			choiceBox5.setValue("a");
 	}
 }
