078512a66b58f905015b048dc98675368edea761
Munevver <uslukilicm@gmail.com>
2020-05-14 14:22:21 +0300

    Auto stash before merge of "user-interface" and "origin/user-interface"

--- tmDesktop/src/main/java/Main.java
+++ tmDesktop/src/main/java/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String s = "abc";
+        s.toUpperCase();
+        System.out.println(s);
         GameUI ui = new GameUI();
         ui.main(args);
     }
