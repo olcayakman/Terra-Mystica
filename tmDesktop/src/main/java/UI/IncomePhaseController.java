2e9c1ee1a7945365ce4bbfca30525eb9f0c2d9b7
Ata Atasoy <h.ataatasoy@gmail.com>
2020-05-18 20:38:02 +0300

    Add executeActionPhase to IncomePhaseController

--- tmDesktop/src/main/java/UI/IncomePhaseController.java
+++ tmDesktop/src/main/java/UI/IncomePhaseController.java
@@ -2,11 +2,13 @@ package UI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import GameLogic.GameHandler;
 
 public class IncomePhaseController {
 
     @FXML
     private void initContinueButtonClicked(ActionEvent event) throws Throwable {
         GameplayController.incomePhaseStage.close();
+        GameHandler.getInstance().executeIncomePhase();
     }
 }
