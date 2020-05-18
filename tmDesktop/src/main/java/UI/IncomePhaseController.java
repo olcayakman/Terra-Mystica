01ba1f19d25134b340e02c6a49b6bcb42dff225e
olcay <olcayakman@hotmail.com>
2020-05-18 21:44:00 +0300

    action stage ready to write

--- tmDesktop/src/main/java/UI/IncomePhaseController.java
+++ tmDesktop/src/main/java/UI/IncomePhaseController.java
@@ -3,8 +3,11 @@ package UI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import GameLogic.GameHandler;
+import javafx.scene.control.Label;
 
 public class IncomePhaseController {
+
+    GameplayController gameplayController = new GameplayController();
 
     @FXML
     private void initContinueButtonClicked(ActionEvent event) throws Throwable {
